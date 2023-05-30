f327df63c649eef7bb92ad9a44666cc0d430cf1a
smurf <smurf@kakao.com>
2023-05-30 21:04:10 +0900

    :lipstick: Input component style added

--- src/components/atoms/Input/Input.jsx
+++ src/components/atoms/Input/Input.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
-styled.input;
-function Input() {
-  return <div>asd</div>;
+const StyledInput = styled.input`
+  width: 100%;
+`;
+
+// props 받아서 polymorphic 컴포넌트 만들기
+function Input(props) {
+  return (
+    <div>
+      <StyledInput />
+    </div>
+  );
 }
 
 export default Input;
