1bad37311cbc9e7f26a1c31feb069aebf5b15e31
smurf <smurf@kakao.com>
2023-05-30 19:21:26 +0900

    :bug: Fix eslint prettier conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Edit</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          test
         </a>
       </header>
     </div>
