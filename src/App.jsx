79f24d2de77804f5d80ec267eae4509d3ce38435
smurf <smurf@kakao.com>
2023-05-30 21:12:47 +0900

    :bulb: Added comment on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
+// 최종출력은 container 폴더안 컴포넌트를 렌더한다.
 function App() {
-  return <div className="App">test</div>;
+  return <div className="App">test2</div>;
 }
 
 export default App;
---
+++ src/pages/mChat.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MChat() {
+  return <div />;
+}
+
+export default MChat;
