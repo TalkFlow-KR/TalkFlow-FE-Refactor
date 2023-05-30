f6997a27a1afa978f94d3da3f58b110860f44801
smurf <smurf@kakao.com>
2023-05-30 20:57:03 +0900

    :sparkles: Styled-Components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
 import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Edit</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          test
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">test</div>;
 }
 
 export default App;
---
+++ src/components/atoms/Input/Input.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+styled.input;
+function Input() {
+  return <div>asd</div>;
+}
+
+export default Input;
