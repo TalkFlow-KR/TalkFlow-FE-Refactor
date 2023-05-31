5012e6ac93cebe461f389fc9ee8022674937b224
smurf <smurf@kakao.com>
2023-06-01 01:46:35 +0900

    :sparkles: added contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Test from "./container/Test";
+import MMain from "./pages/mobile/mMain";
 
 // 최종출력은 container 폴더안 컴포넌트를 렌더한다.
 function App() {
   return (
     <div className="App">
       <Test />
-      <mMain />
+      <hr />
+      <MMain />
     </div>
   );
 }
--- src/pages/mobile/mMain.jsx
+++ src/pages/mobile/mMain.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 function MMain() {
-  return <div />;
+  // mChat 부분 테스트
+  return (
+    <>
+      {/* 모바일 헤더 부분 */}
+      <header>
+        <button type="button">뒤로가기 버튼</button>
+        <h1>
+          <a href="#!">로고이미지</a>
+        </h1>
+      </header>
+      {/* 모바일 채팅 부분 */}
+      <main>
+        <section>
+          <article>
+            <h2>Room Title</h2>
+          </article>
+          <article>
+            <div>파트너 말풍선</div>
+            <div>상대 말풍선</div>
+          </article>
+        </section>
+      </main>
+      <footer>
+        <div>
+          <input type="text" placeholder="다음으로 넘어가세요!" />
+        </div>
+        <div>
+          <button type="button">마이크이미지 </button>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default MMain;
