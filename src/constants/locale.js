d272c222597a45179f4957ecb03682404225a7ee
smurf <smurf@kakao.com>
2023-06-01 01:47:13 +0900

    :speech_balloon: added text literals

---
+++ src/constants/locale.js
@@ -0,0 +1,22 @@
+const locale = {
+  btn: {
+    type: {
+      button: "button",
+      submit: "submit",
+      reset: "reset",
+    },
+    string: {
+      button: "버튼",
+      submit: "제출",
+      reset: "초기화",
+    },
+  },
+  input: {
+    placeholder: {
+      typeMessage: "메세지를 입력하세요.",
+      typeUserId: "아이디를 입력하세요.",
+      typePassword: "패스워드를 입력하세요.",
+    },
+  },
+};
+export default locale;
