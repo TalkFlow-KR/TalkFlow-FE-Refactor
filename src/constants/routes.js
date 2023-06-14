5d247c654c243af2e73d749baec28b75c1e9579f
smurf <smurf@kakao.com>
2023-06-14 17:40:52 +0900

    :sparkles: added constants files

---
+++ src/constants/routes.js
@@ -0,0 +1,20 @@
+const mobile = [
+  {
+    path: "/",
+    component: "",
+  },
+];
+
+const desktop = [
+  {
+    path: "/",
+    component: "",
+  },
+];
+
+const routes = {
+  mobile,
+  desktop,
+};
+
+export default routes;
