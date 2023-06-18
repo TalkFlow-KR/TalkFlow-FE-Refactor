375dd2fc257f02b9dba69d3bf2e6f8415d56a06c
smurf <smurf@kakao.com>
2023-06-19 01:05:50 +0900

    :sparkles: modified input component more polymorphic

--- src/components/atoms/Input/input.jsx
+++ src/components/atoms/Input/input.jsx
@@ -2,10 +2,26 @@ import React from "react";
 import { Container, StyledInput } from "./input.styled";
 
 // props 받아서 polymorphic 컴포넌트 만들기
-function Input(props) {
+function Input({ variant, onChange, onKeydown, ...rest }) {
+  const handleChange = (e) => {
+    if (onChange) {
+      onChange(e);
+    }
+  };
+  const handleKeydown = (e) => {
+    if (onKeydown) {
+      onKeydown(e);
+    }
+  };
   return (
     <Container>
-      <StyledInput type="text" placeholder="sample" />
+      <StyledInput
+        type={variant ? variant.TYPE : "text"}
+        placeholder={variant ? variant.PLACEHOLDER : ""}
+        onChange={handleChange}
+        onKeyDown={handleKeydown}
+        {...rest}
+      />
     </Container>
   );
 }
