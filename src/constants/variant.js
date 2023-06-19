f7316de1fd1fb2f5e5f1f41fe26ca020f969415b
smurf <smurf@kakao.com>
2023-06-20 08:36:53 +0900

    :wrench: modified variant.js

--- src/constants/variant.js
+++ src/constants/variant.js
@@ -16,6 +16,12 @@ export const INPUT_TYPE = Object.freeze({
     TEXT: "password",
     PLACEHOLDER: "PASSWORD",
   },
+  SHOW_PASSWORD_INPUT: {
+    ID: "pw",
+    TYPE: "text",
+    TEXT: "password",
+    PLACEHOLDER: "PASSWORD",
+  },
   MESSAGE_INPUT: {
     ID: "textarea",
     TYPE: "textarea",
