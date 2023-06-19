d95055b7a694994f603abe420a481c6de589d0ef
smurf <smurf@kakao.com>
2023-06-20 08:36:53 +0900

    :lipstick: edited button styling

--- src/components/atoms/Button/button.jsx
+++ src/components/atoms/Button/button.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import styled from "styled-components";
 import { BUTTON_TYPE } from "../../../constants/variant";
 
+export const StyledButton = styled.button``;
 function Button({ children, variant }) {
   const type = variant || BUTTON_TYPE.BUTTON;
-  return <button type={type}>{children}</button>;
+  return <StyledButton type={type}>{children}</StyledButton>;
 }
 
 export default Button;
