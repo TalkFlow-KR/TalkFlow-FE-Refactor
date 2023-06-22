285427f9440b3853d62e70e08ebe692542d56d8b
smurf <smurf@kakao.com>
2023-06-22 18:03:53 +0900

    :sparkles: added Signup texts locale.js

--- src/constants/locale.js
+++ src/constants/locale.js
@@ -1,4 +1,24 @@
-const locale = {
+const LOCALE = {
+  SIGNUP: {
+    TITLE: "ABC FLOW 회원가입 하기",
+    SUBTITLE: "ABC FLOW 오신것을 환영합니다",
+    TEXT: "지금 바로 회원가입하고, 무료로 서비스를 이용해보세요.",
+    ASK_LOGIN: "이미 회원이신가요 ?",
+    LOGIN: "로그인 하기",
+    SIGNUP_BUTTON: "계정 생성하기",
+    AGREE_TERMS: "회원가입과 동시에 약관에 동의합니다.",
+    PASSWORD: "패스워드",
+    USERNAME: "닉네임",
+    NAME: "성함",
+    EMAIL: "이메일",
+    ERROR: {
+      USER_NAME_EMPTY: "닉네임은 공백이 될 수 없습니다.",
+      EMAIL_ERROR: "이메일 주소 구성이 잘 못 되었습니다.",
+      NAME_EMPTY: "성함은 공백이 될 수 없습니다.",
+      PW_SHORT: "비밀번호가 너무 짧습니다. (6글자 이상)",
+      AGREE_TERMS: "약관에 동의하셔야 합니다.",
+    },
+  },
   btn: {
     type: {
       button: "button",
@@ -19,4 +39,4 @@ const locale = {
     },
   },
 };
-export default locale;
+export default LOCALE;
