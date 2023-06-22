742cfb6a950dc8476a41a7e08fbbc8c286807916
smurf <smurf@kakao.com>
2023-06-22 19:00:18 +0900

    :sparkles: added input string register box

--- src/constants/variant.js
+++ src/constants/variant.js
@@ -2,20 +2,40 @@ export const BUTTON_TYPE = Object.freeze({
   BUTTON: "button",
 });
 export const INPUT_TYPE = Object.freeze({
+  USERNAME_INPUT: {
+    ID: "uname",
+    NAME: "uname",
+    TYPE: "text",
+    PLACEHOLDER: "USERNAME",
+  },
+  NAME_INPUT: {
+    ID: "rname",
+    NAME: "rname",
+    TYPE: "text",
+    PLACEHOLDER: "NAME",
+  },
   EMAIL_INPUT: {
     ID: "email",
+    NAME: "email",
     TYPE: "text",
     TEXT: "email",
     PLACEHOLDER: "EMAIL",
   },
-  CHECKBOX: "checkbox",
-  TEXT: "text",
   PASSWORD_INPUT: {
     ID: "pw",
+    NAME: "pw",
     TYPE: "password",
-    TEXT: "password",
+    TEXT: "PASSWORD",
     PLACEHOLDER: "PASSWORD",
   },
+  CHECKBOX_INPUT: {
+    ID: "checkbox",
+    NAME: "checkbox",
+    TYPE: "checkbox",
+    TEXT: "CHECKBOX",
+    PLACEHOLDER: "CHECKBOX",
+  },
+  TEXT: "text",
   SHOW_PASSWORD_INPUT: {
     ID: "pw",
     TYPE: "text",
